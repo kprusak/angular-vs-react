44f52d2856a62d1f77a9310287bf6840d8ea1ed6
kprusak <konrad.prusak@gmail.com>
2022-05-08 14:32:47 +0200

    changed graphics, set graphic size

--- react-app/src/components/big-item/big-item.js
+++ react-app/src/components/big-item/big-item.js
@@ -1,11 +1,14 @@
 import './big-item.css';
 import React from 'react';
-import image from './graphic.jpg'
+import image from './hot-tasty-pizza.jpg'
 
 export default class BigItem extends React.Component {
     render() {
         return (
-            <img src={image} alt="big item" />
+            <div>
+                <img src={image} alt="big item"  width="1500px" height='900px' />
+                <a href="https://www.freepik.com/photos/pizza-table">Pizza table photo created by Racool_studio - www.freepik.com</a>
+            </div>
         );
     }
 }
